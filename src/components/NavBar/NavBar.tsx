01b85743b2cc5eea193e36644b69acb79f06c494
Mohamed-Mos-aad <ma3268787@gmail.com>
2024-12-27 21:58:25 +0200

    NavBar component structure and style finished

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,30 @@
+import style from '../../style/components/navbar/navbar.module.css'
+import logo from '../../assets/landingPage/landingPageLogo.svg'
+
+
+
+
 export default function NavBar() {
     return (
         <>
-            <nav>
-                Nav Bar
+            <nav className={style.nav}>
+                <div className={style.nav_container}>
+                    <div className={style.logo}>
+                        <img src={logo} alt="" />
+                    </div>
+                    <div className={style.menu}>
+                        <ul>
+                            <li className={style.active_section}>نبذة عنّا</li>
+                            <li>خدماتنا</li>
+                            <li>تواصل معنا</li>
+                            <li>تقييمات المستخدمين</li>
+                        </ul>
+                    </div>
+                    <div className={style.auth_btns}>
+                        <button>إنشاء حساب</button>
+                        <button>تسجيل دخول</button>
+                    </div>
+                </div>
             </nav>
         </>
     )
--- src/layouts/LandingPageLayout.tsx
+++ src/layouts/LandingPageLayout.tsx
@@ -1,5 +1,5 @@
 import style from '../style/layouts/landingPageLayout.module.css'
-import NavBar from "../components/NavBar/NavBar";
+import NavBar from "../components/navbar/NavBar";
 import { Outlet } from "react-router-dom";
 
 export default function LandingPageLayout() {
