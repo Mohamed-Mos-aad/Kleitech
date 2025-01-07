0641b20bb27510fb349bd2fb8394003dcfb7e43c
Mohamed-Mos-aad <ma3268787@gmail.com>
2025-01-07 21:08:56 +0200

    Join Us Structure , Style and Logic Finished

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,10 +1,12 @@
 import AboutUs from "./sections/AboutUs";
 import ContactUs from "./sections/ContactUs";
+import JoinUs from "./sections/JoinUs";
 
 export default function LandingPage() {
     return (
         <>
             <AboutUs />
+            <JoinUs />
             <ContactUs />
         </>
     )
