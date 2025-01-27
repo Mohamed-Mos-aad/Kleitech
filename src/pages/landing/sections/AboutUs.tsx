20e4b0ca6bdadcb93deb94195852d0fc92de1dde
Ibrahim Mahrous <ebraheemalimahrous000@gmail.com>
2025-01-27 16:43:31 +0200

    committed change

--- src/pages/landing/sections/AboutUs.tsx
+++ src/pages/landing/sections/AboutUs.tsx
@@ -3,31 +3,22 @@ import aboutImage from '../../../assets/images/about.png';
 import line from '../../../assets/images/line.png';
 import happyClients from '../../../assets/images/happy-clients.png';
 
-// ** InterFaces
-interface ISection{
-    sectionId: string
-}
+// ** Interfaces
 
-export default function AboutUs({sectionId}:ISection) {
+export default function AboutUs() {
     return (
-        <>
-<<<<<<< HEAD
-            <section className={style.section} id={sectionId}>
-                
-            </section>
-=======
-            <div className={style.about_section}>
-                <div className={style.about_container}>
-                    {/* Happy Clients Image */}
-                    <img className={style.happy_clients} src={happyClients} alt="Happy Clients" />
+        <div className={style.about_section}>
+            <div className={style.about_container}>
+                {/* Happy Clients Image */}
+                <img className={style.happy_clients} src={happyClients} alt="Happy Clients" />
 
-                    {/* About Image */}
-                    <div className={style.about_img}>
+                {/* About Image */}
+                <div className={style.about_img}>
                     <img src={aboutImage} alt="About Us" />
-                    </div>
+                </div>
 
-                    {/* About Content */}
-                    <div className={style.about_content}>
+                {/* About Content */}
+                <div className={style.about_content}>
                     <h2>شريكك الموثوق لدعم صحتك الرقمية:</h2>
                     <p>
                         في <span>كليّتِك</span> نقدم حلولاً رقمية مبتكرة لدعم مرضى الكلى، مما يتيح لك إمكانية تصوير ورفع صور الأشعة
@@ -38,12 +29,8 @@ export default function AboutUs({sectionId}:ISection) {
                     </p>
                     {/* Line Overlay */}
                     <img src={line} alt="Decorative Line" className={style.line_image} />
-                    </div>
-                    <div>I love coding enrj </div>
                 </div>
             </div>
-
->>>>>>> About-Us
-        </>
-    )
+        </div>
+    );
 }
