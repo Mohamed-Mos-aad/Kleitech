91aab46d0ff0083a7be63153eb5fcd747ffee390
Mohamed-Mos-aad <ma3268787@gmail.com>
2025-01-31 22:53:24 +0200

    Otp screen structure and style finished

---
+++ src/pages/auth/Otp.tsx
@@ -0,0 +1,42 @@
+// ** Assets
+import logo from '../../assets/auth/otpIcon.svg'
+// ** Style
+import style from '../../style/pages/auth/otp.module.css'
+
+
+
+
+
+export default function Otp() {
+    return (
+        <>
+            <div className={style.otp_container}>
+                <div className={style.icon}>
+                    <img src={logo} alt="Logo icon" />
+                </div>
+                <div className={style.content}>
+                    <h2>التحقق من الكود</h2>
+                    <p>
+                        الرجاء ادخال الكود الذي ارسلناه الي البريد الالكتروني 
+                        klity******@gamil.com
+                    </p>
+                </div>
+                <form>
+                    <div className={style.timer}>
+                        00:30
+                    </div>
+                    <div className={style.otp_code}>
+                        <input type="text" />
+                        <input type="text" />
+                        <input type="text" />
+                        <input type="text" />
+                        <input type="text" />
+                        <input type="text" />
+                    </div>
+                    <button>إرسال رمز التأكيد</button>
+                    <h4>لم يتم ارسال رمز تأكيد ؟<span> ارسل مره اخري</span></h4>
+                </form>
+            </div>
+        </>
+    )
+}
