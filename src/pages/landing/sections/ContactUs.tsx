fbc8c19efe17310bc28415274a637b72a7a2699f
Mohamed-Mos-aad <ma3268787@gmail.com>
2025-02-11 17:48:04 +0200

    update some code of landing page

--- src/pages/landing/sections/ContactUs.tsx
+++ src/pages/landing/sections/ContactUs.tsx
@@ -22,30 +22,30 @@ export default function ContactUs({sectionId}:ISection) {
                         <form className={style.contact_us_form}>
                             <div className={style.form_row}>
                                 <div className={style.form_input}>
-                                    <label htmlFor="">الاسم الاول</label>
-                                    <input type="text" placeholder='ادخل الإسم  الاول'/>
+                                    <label htmlFor="firstName">الاسم الاول</label>
+                                    <input type="text" placeholder='ادخل الإسم  الاول' id='firstName'/>
                                 </div>
                                 <div className={style.form_input}>
-                                    <label htmlFor="">الاسم الثاني</label>
-                                    <input type="text" placeholder='ادخل الإسم  الثاني'/>
+                                    <label htmlFor="secondName">الاسم الثاني</label>
+                                    <input type="text" placeholder='ادخل الإسم  الثاني' id='secondName'/>
                                 </div>
                             </div>
                             <div className={style.form_input}>
-                                <label htmlFor="">البريد الالكتروني</label>
-                                <input type="text" placeholder='ادخل البريد الالكتروني'/>
+                                <label htmlFor="email">البريد الالكتروني</label>
+                                <input type="email" placeholder='ادخل البريد الالكتروني' id='email'/>
                             </div>
                             <div className={style.form_input}>
-                                <label htmlFor="">الرساله</label>
-                                <textarea name="" id="" placeholder='اكتب رسالتك'></textarea>
+                                <label htmlFor="message">الرساله</label>
+                                <textarea name="" id="message" placeholder='اكتب رسالتك'></textarea>
                             </div>
                             <div className={style.form_terms}>
-                                <input type="checkbox" name="" id="" />
-                                <label htmlFor="">انت توافق علي <a href="">سياسيه الخصوصيه</a> الخاصه بنا</label>
+                                <input type="checkbox" name="" id="checkbox" />
+                                <label htmlFor="checkbox">انت توافق علي <a href="">سياسيه الخصوصيه</a> الخاصه بنا</label>
                             </div>
                             <button>إرسال الرساله</button>
                         </form>
                         <div className={style.contact_photo}>
-                            <img src={contactPhoto} alt="" />
+                            <img src={contactPhoto} alt="Contact photo" />
                         </div>
                     </div>
                 </div>
