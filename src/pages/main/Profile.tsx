905ce846090886694ccdcc210bac3b539648493d
Mohamed-Mos-aad <ma3268787@gmail.com>
2025-04-14 11:13:00 +0200

    Profile Page Structure , style and logic finished

---
+++ src/pages/main/Profile.tsx
@@ -0,0 +1,154 @@
+// ** Style
+import style from '../../style/pages/main/profile.module.css'
+// ** Assets
+import settingIcon from '../../assets/navBar/settingIcon.svg'
+import doctorPhoto from '../../assets/landingPage/PatientReviews/PatientReview-1.png'
+import editeIcon from '../../assets/main/profile/editeIcon.svg'
+import userNameIcon from '../../assets/auth/formIcons/userNameIcon.svg'
+import userPhoneIcon from '../../assets/auth/formIcons/userPhoneIcon.svg'
+import userIdIcon from '../../assets/auth/formIcons/userIdIcon.svg'
+import userEmailIcon from '../../assets/auth/formIcons/userEmailIcon.svg'
+import arrowUpandDownIcon from '../../assets/auth/formIcons/arrowsUpandDownIcon.svg'
+// ** Hooks
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import InputElement from '../../components/ui/InputElement'
+
+
+
+
+
+export default function Profile() {
+    // ** Default
+    const navigate = useNavigate();
+
+
+
+
+
+    // ** States
+    const [settingOpened,setSettingOpened] = useState<boolean>(false);
+    const [editeOpened,setEditeOpened] = useState<boolean>(false);
+
+
+
+    // ** Handlers
+    const settingToggelHandler = ()=>{setSettingOpened(!settingOpened)};
+    const changePasswordPageHandler = ()=>{navigate('/u/new-password')}
+    const editingToggelHanlder = ()=>{setEditeOpened(!editeOpened)};
+
+
+
+
+    return (
+        <>
+            <div className={style.profile_container}>
+                <section>
+                    <div className={style.profile_img}>
+                        <img src={doctorPhoto} alt="" />
+                    </div>
+                    <div className={style.user_short_data}>
+                        <h1>محمد يوسف</h1>
+                        <p>القاهره,الدقهليه,المنصوره </p>
+                    </div>
+                    <div className={style.setting_btn}>
+                        <img src={settingIcon} alt="Setting icon" onClick={settingToggelHandler}/>
+                        {
+                            settingOpened &&
+                            <div className={style.setting_list}>
+                                <ul>
+                                    <li onClick={changePasswordPageHandler}>تعديل كلمة المرور</li>
+                                </ul>
+                            </div>
+                        }
+                    </div>
+                </section>
+                {
+                    editeOpened ?
+                    <section className={style.editing_section}>
+                        <div className={style.user_data}>
+                            <ul>
+                                <li>
+                                    <InputElement type='text' id='' img={{src: userNameIcon,alt:'UserName icon'}} name='الإسم'  value='' error=''  onChange={()=>{console.log('work')}} placeholder='' key={1}/>
+                                </li>
+                                <li>
+                                    <InputElement type='text' id='' img={{src: userPhoneIcon,alt:'UserPhone icon'}} name='رقم الهاتف'  value='' error=''  onChange={()=>{console.log('work')}} placeholder='' key={2}/>
+                                </li>
+                                <li>
+                                    <InputElement type='text' id='' img={{src: userIdIcon,alt:'UserId icon'}} name='الرقم القومي'  value='' error=''  onChange={()=>{console.log('work')}} placeholder='' key={3}/>
+                                </li>
+                                <li>
+                                    <InputElement type='text' id='' img={{src: userEmailIcon,alt:'UserEmail icon'}} name='البريد الالكتروني'  value='' error=''  onChange={()=>{console.log('work')}} placeholder='' key={4}/>
+                                </li>
+                                <li>
+                                    <InputElement type='text' id='' img={{src: arrowUpandDownIcon,alt:'arrowUpandDown icon'}} name='الوزن'  value='' error=''  onChange={()=>{console.log('work')}} placeholder='' key={5}/>
+                                </li>
+                                <li>
+                                    <InputElement type='text' id='' img={{src: arrowUpandDownIcon,alt:'arrowUpandDown icon'}} name='الطول'  value='' error=''  onChange={()=>{console.log('work')}} placeholder='' key={6}/>
+                                </li>
+                            </ul>
+                        </div>
+                        <button onClick={editingToggelHanlder}>حفظ التغيرات</button>
+                    </section>
+                    :
+                    <>
+                        <section>
+                            <div className={style.edite_btn} onClick={editingToggelHanlder}>
+                                تعديل
+                                <img src={editeIcon} alt="Edite icon"/>
+                            </div>
+                            <div className={style.section_title}>
+                                <h2>المعلومات الشخصيه </h2>
+                            </div>
+                            <div className={style.user_data}>
+                                <ul>
+                                    <li>
+                                        <h3>الإسم</h3>
+                                        <p>محمد يوسف ابراهيم احمد</p>
+                                    </li>
+                                    <li>
+                                        <h3>رقم الهاتف</h3>
+                                        <p>01283529923</p>
+                                    </li>
+                                    <li>
+                                        <h3>الرقم القومي</h3>
+                                        <p>30208051102202</p>
+                                    </li>
+                                    <li>
+                                        <h3>البريد الالكتروني</h3>
+                                        <p>mohamedyousef178@gmail.com</p>
+                                    </li>
+                                    <li>
+                                        <h3>الوزن</h3>
+                                        <p>82</p>
+                                    </li>
+                                    <li>
+                                        <h3>الطول</h3>
+                                        <p>190</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </section>
+                        <section>
+                            <div className={style.section_title}>
+                                <h2>العنوان</h2>
+                            </div>
+                            <div className={style.user_data}>
+                                <ul>
+                                    <li>
+                                        <h3>الدوله</h3>
+                                        <p>القاهره</p>
+                                    </li>
+                                    <li>
+                                        <h3>المحافظه / البلد</h3>
+                                        <p>الدقهليه/المنصوره</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </section>
+                    </>
+                }
+            </div>
+        </>
+    )
+}
